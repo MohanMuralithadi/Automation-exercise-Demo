077035dea9fa81e6dc643c8142ebe2d9df8b67c2
Mohan Murali Thadi <mthadi@student.gitam.edu>
2026-05-06 17:55:52 +0530

    new

--- Automationexercise/automationexercise-ecommerce.spec.ts
+++ Automationexercise/automationexercise-ecommerce.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright';
+import { expect, test } from '@playwright/test';
 import { AccountPage } from './pages/AccountPage';
 import { CartPage } from './pages/CartPage';
 import { CheckoutPage } from './pages/CheckoutPage';
